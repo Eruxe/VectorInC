244329fac8cc0c47494531da7ebb31318063810f
Neuring <36540897+Recursived@users.noreply.github.com>
2019-11-25 15:31:55 +0100

    Ajout des fichiers générateurs de random

---
+++ random.c
@@ -0,0 +1,11 @@
+#include "random.h"
+#include <time.h>
+#include <stdlib.h>
+
+srand(time(NULL));
+
+
+double random_double(double a, double b){
+
+}
+
---
+++ random.h
@@ -0,0 +1,14 @@
+#ifndef __RANDOM__
+#define __RANDOM__
+
+#include <ctypes.h>
+
+double random_double(double a, double b);
+float random_float(float a, float b);
+size_t random_size_t(size_t a, size_t b);
+int random_int(int a, int b);
+unsigned char random_uchar(unsigned char a, unsigned char b);
+void random_init_string(unsigned char *c, size_t n);
+
+
+#endif
