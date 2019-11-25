a66d7a49ea457393932337a6d3f1adc2cb8cc236
Eruxe <39292686+Eruxe@users.noreply.github.com>
2019-11-25 16:38:33 +0100

    test random string

--- test_random.c
+++ test_random.c
@@ -9,6 +9,11 @@ int main(int argc, char *argv[]){
 	printf("int : %d \n", random_int(1,5));
 	printf("size_t : %d \n", random_size_t(1,5));
 	printf("size_t : %c \n", random_uchar('a','e'));
+	
+	size_t size_c = 10;
+	unsigned char * c = malloc(sizeof(unsigned char)*size_c);
+	random_init_string(c, size_c);
+	printf("string: %s \n", c);
 
 	return 0;
 }
