f108897e31bb60cca0536dd8bbbb77af88bc703c
Neuring <36540897+Recursived@users.noreply.github.com>
2020-01-06 22:25:43 +0100

    Update vector_v2_double.c

--- vector_v2_double.c
+++ vector_v2_double.c
@@ -46,9 +46,7 @@ double get(p_s_vector_v2_double p_vector, size_t i){
 }
 
 void vector_v2_double_insert(p_s_vector_v2_double p_vector, size_t i, double v){
-
-	// TO - DO : faire la fonction	
-	
+	// TO - DO : refaire la fonction	
 }
 
 void vector_v2_double_erase(p_s_vector_v2_double p_vector, size_t i){
@@ -60,20 +58,31 @@ void vector_v2_double_push_back(p_s_vector_v2_double p_vector, double v){
 }
 
 void vector_v2_double_pop_back(p_s_vector_v2_double p_vector){
-	// TO - DO : refaire la fonction	
+	// TO - DO : refaire la fonction
 }
 
 void vector_v2_double_clear(p_s_vector_v2_double p_vector){
-	// TO - DO : refaire la fonction	
+	p_vector->size = 0;
+	p_vector->capacity = 16;
+	
+	double *temp = realloc(p_vector->data, (p_vector->capacity) * sizeof(double));
+	if (temp != null){
+		p_vector->data = temp;
+	}
+	
+	// On reinitialise les valeurs à 0
+	for (int i = 0; i < 16; i++){
+		p_vector->data[i] = 0.0;
+	}
  
 }
 
 int vector_v2_double_empty(p_s_vector_v2_double p_vector){
-	// TO - DO : refaire la fonction	
+	return p_vector->size == 0;	
 
 }
 
 size_t vector_v2_double_size(p_s_vector_v2_double p_vector){
-	return p_vector->capacity; // pour renvoyer le nb de slot utilise faire : p_vector->size;
+	return p_vector->capacity; // pour renvoyer le nb de slot utilisé faire : p_vector->size;
 }
 
