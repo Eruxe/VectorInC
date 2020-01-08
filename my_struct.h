b932dbc531579da538d728c5899ad7adb64d8bac
Eruxe <39292686+Eruxe@users.noreply.github.com>
2020-01-08 14:55:01 +0100

    Definition des fonctions dans le h

--- my_struct.h
+++ my_struct.h
@@ -8,4 +8,14 @@ typedef struct {
 
 typedef s_my_struct* p_s_my_struct;
 
+p_s_my_struct my_struct_alloc();
+
+void my_struct_free(p_s_my_struct p_vector);
+
+my_struct_randoms_init(p_s_my_struct p_vector);
+
+void my_struct_copy(p_s_my_struct p_dest, p_s_my_struct p_src);
+
+int my_struct_cmp(p_s_my_struct p_vector_a, p_s_my_struct p_vector_b);
+
 #endif
