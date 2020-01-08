1cdbe9d192e6c836130de9094b8495f09b534b4f
Eruxe <39292686+Eruxe@users.noreply.github.com>
2020-01-08 13:15:11 +0100

    LA CEST BON LA ?

--- vector_v1_double.c
+++ vector_v1_double.c
@@ -71,20 +71,25 @@ void vector_v1_double_insert(p_s_vector_v1_double p_vector, size_t i, double v){
 void vector_v1_double_erase(p_s_vector_v1_double p_vector, size_t i){
 	// On copie la data puis on realloue
 	if (i < p_vector->size && p_vector->size > 1){
-		double *temp = malloc(sizeof(double) * (--p_vector->size));
 
-		for (size_t k = 0; k < p_vector->size+1; k++){
-			if (k < i){
-				temp[k] = p_vector->data[k];
-			} else if (k > i){
-				temp[k] = p_vector->data[k+1];
-			}
+		for (size_t k = i; k < p_vector->size-1; k++){
+			p_vector->data[k] = p_vector->data[k+1];
 		}
 
+		p_vector->size-=1;
+		double *arr = malloc(sizeof(double) * p_vector->size);
+		for (int i = 0; i < p_vector->size; i++){
+			arr[i] = p_vector->data[i];
+		}
+		p_vector->data = arr;
+
+		//p_vector->data = realloc(p_vector->data, sizeof(double)*p_vector->size);
+		
 		
 
 	} else if (i < p_vector->size && p_vector->size == 1){
 		p_vector->data = NULL;
+		p_vector->size = 0;
 	}
 }
 
