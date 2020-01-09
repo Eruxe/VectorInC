8834926321b8c5814223f33ad74637584f79748b
Neuring <36540897+Recursived@users.noreply.github.com>
2020-01-09 18:14:36 +0100

    Update test_my_struct.c

--- test_my_struct.c
+++ test_my_struct.c
@@ -1,7 +1,19 @@
-#include "vector_v1_double.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "my_struct.h"
+#include <assert.h>
+#include <time.h>
 
-int main(int argc, char *argv[]){
-
-
-    return 0;
+int main(void) {
+  srand(time(0));
+  p_s_my_struct p = my_struct_alloc();
+  assert(p->chaine == NULL);
+  assert(p->nb == 0);
+  my_struct_randoms_init(p);
+  assert(p->nb != 0);
+  assert(p->chaine != NULL);
+  p_s_my_struct p1 = my_struct_alloc();
+  my_struct_copy(p1, p);
+  assert(my_struct_cmp(p, p1)==0);
+  return 0;
 }
