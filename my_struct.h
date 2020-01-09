06cd7d705f2f01ba7585f298f6341ab75d8bca96
Neuring <36540897+Recursived@users.noreply.github.com>
2020-01-09 17:44:20 +0100

    Correction type retour

--- my_struct.h
+++ my_struct.h
@@ -12,7 +12,7 @@ p_s_my_struct my_struct_alloc();
 
 void my_struct_free(p_s_my_struct p_vector);
 
-my_struct_randoms_init(p_s_my_struct p_vector);
+void my_struct_randoms_init(p_s_my_struct p_vector);
 
 void my_struct_copy(p_s_my_struct p_dest, p_s_my_struct p_src);
 
